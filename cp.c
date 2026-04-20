5ab162f52ac9007dd8482a3913f5691c4b0da5cb
Enzo Carrara <enzoebc1@gmail.com>
2026-04-19 22:05:58 -0300

    Fix variable name for file size in cp.c

--- cp.c
+++ cp.c
@@ -1,4 +1,3 @@
-#include<stdio.h>
 #include<stdlib.h>
 #include<unistd.h>
 #include<fcntl.h>
@@ -9,16 +8,16 @@ int main(int argc, char** argv)
     int src = open(argv[1], O_RDONLY);
     int dst = open(argv[2], O_WRONLY | O_CREAT, 0644);
 
-    off_t tamanho = lseek(src, 0, SEEK_END);
-    char* memory = (char*)malloc(tamanho);
+    off_t size = lseek(src, 0, SEEK_END);
+    char* memory = (char*)malloc(size);
     lseek(src, 0, SEEK_SET);
 
-    ssize_t bytes = read(src, memory, tamanho);
+    ssize_t bytes = read(src, memory, size);
 
     while(bytes > 0)
     {
         write(dst, memory, bytes);
-        bytes = read(src, memory, tamanho);
+        bytes = read(src, memory, size);
     }
 
     close(src);
